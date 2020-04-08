a41e2a7a61a61242edd9249cc26a6c8fc67e9a23
ShrutiAggarwal99 <31125152+ShrutiAggarwal99@users.noreply.github.com>
2020-04-08 13:46:52 +0530

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,20 @@
+int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size(), minDiff = INT_MAX, ans = 0;
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<n;i++){
+            int st = i+1, en = n-1;
+            while(st<en){
+                int curSum = nums[i] + nums[st] +nums[en];
+                int curDiff = abs(curSum-target);
+                if(minDiff > curDiff){
+                    minDiff = curDiff;
+                    ans = curSum;
+                }
+                if(curSum<target) st++;
+                else if(curSum>target) en--;
+                else return target;
+            }
+        }
+        return ans;
+    }
