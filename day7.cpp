f2bfcdd47419d73bdb82dc34819d3cc26faad8e3
ShrutiAggarwal99 <31125152+ShrutiAggarwal99@users.noreply.github.com>
2020-04-12 11:59:43 +0530

    Update day7.cpp

--- day7.cpp
+++ day7.cpp
@@ -1,1 +1,5 @@
+// Link to problem - https://www.linkedin.com/posts/women-who-code-delhi_hi-everyone-here-is-the-question-for-day-activity-6654626615886213120-iNnF
 
+bool amusementPark(vector<vector<int> > &rides){
+  
+}
